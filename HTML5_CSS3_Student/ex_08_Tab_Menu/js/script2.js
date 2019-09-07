7c22e4985711ee7acdc871a0a9a3d0fa054d39cf
jushik91 <jushik91@gmail.com>
2019-09-07 21:05:20 +0900

    타이타닉

---
+++ HTML5_CSS3_Student/ex_08_Tab_Menu/js/script2.js
@@ -0,0 +1,26 @@
+﻿/* alert('a'); */
+jQuery(document).ready(function(){
+	$('.item1').show();
+	$('.item2').hide();
+	$('.button1').css('background','orange');
+	$('.button2').css('background','black');
+	
+	$('.button2').click(function() {
+		$('.item2').show();
+		$('.item1').hide();
+		$('.button2').css('background','orange');
+		$('.button1').css('background','black');
+	});
+
+	
+	$('.button1').click(function() {
+		$('.item1').show();
+		$('.item2').hide();
+		$('.button1').css('background','orange');
+		$('.button2').css('background','black');
+	});	
+	
+	
+});
+
+
